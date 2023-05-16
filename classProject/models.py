d6b177ba6fe8b3527292c058b9084020eaf7e6a0
bekirbali <bfbali43@gmail.com>
2023-05-16 21:45:10 +0300

    models, admin, urls, views

--- classProject/models.py
+++ classProject/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=35)
+    number = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    joined_date = models.DateTimeField(auto_now_add=True) #kayıt yapıldığı an otomatik yaz
+    updated_date = models.DateTimeField(auto_now=True) # kayıt güncellendiği an otomatik yaz
+    email = models.EmailField()
+    # image = models.ImageField()
+
+    def __str__(self):
+       return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        verbose_name = 'The Students'
+        verbose_name_plural = 'The Students'
+   
