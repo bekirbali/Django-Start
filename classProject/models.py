eee8c17a11ea0f5e20e8000fe8accbfd21d0861b
bekirbali <bfbali43@gmail.com>
2023-05-18 15:48:31 +0300

    default, null

--- classProject/models.py
+++ classProject/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 class Student(models.Model):
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=35)
+    last_name = models.CharField(max_length=35, null=True, blank=True)
     number = models.IntegerField(default=0)
     is_active = models.BooleanField(default=True)
     joined_date = models.DateTimeField(auto_now_add=True) #kayıt yapıldığı an otomatik yaz
     updated_date = models.DateTimeField(auto_now=True) # kayıt güncellendiği an otomatik yaz
     email = models.EmailField()
-    image = models.ImageField(upload_to='images/', default='')
+    image = models.ImageField(upload_to='images/', default='' null=True, blank=True)
 
     def __str__(self):
        return f'{self.first_name} {self.last_name}'
