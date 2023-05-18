04f6f68fa15e028bc8f17e5967d7362b5160a016
bekirbali <bfbali43@gmail.com>
2023-05-18 16:28:25 +0300

    testing db

--- firstProject/urls.py
+++ firstProject/urls.py
@@ -26,3 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('classProject.urls'))
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
