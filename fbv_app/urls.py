b5b4bb8fa64c2b6116e6e0656c18651a4ddc8a87
bekirbali <bfbali43@gmail.com>
2023-05-19 21:38:09 +0300

    views

--- fbv_app/urls.py
+++ fbv_app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import *
+from .views import (
+    home,
+    student_list,
+    student_create
+)
 
 urlpatterns = [
     path('', home),
+    path('student_list/', student_list),
+    path('student_create/', student_create),
 ]
