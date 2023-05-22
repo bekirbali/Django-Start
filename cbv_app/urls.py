30581cedfd620372f867cf5fc7ee6bd8ba3ccb6f
bekirbali <bfbali43@gmail.com>
2023-05-22 16:19:14 +0300

    new app, views post get

--- cbv_app/urls.py
+++ cbv_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from .views import (
+    Student_list
+)
 
 urlpatterns = [
-    path('', home)
+    path('student_list/', Student_list.as_view())
 ]
--- cbv_app/views.py
+++ cbv_app/views.py
@@ -3,6 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework.views import APIView
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 def home(request):
     pass
+
+class Student_list(APIView):
+    def get(self, request):
+        students = Student.objects.all()
+        serializer = StudentSerializer(instance=students, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
